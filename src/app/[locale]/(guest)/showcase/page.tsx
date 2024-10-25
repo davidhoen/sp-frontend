c17c29b2a4741d55a1f18e1b31c601f89b551975
DavidHoen <david.hoen10@gmail.com>
2024-10-25 14:07:30 +0200

    Added FeedbackCard

--- src/app/[locale]/(guest)/showcase/page.tsx
+++ src/app/[locale]/(guest)/showcase/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import { FeedbackCard } from "@/components/FeedbackCard"
 import UserProfile from "@/components/navigation/UserProfile"
 import StarRating from "@/components/StarRating"
 import PageTitle from "@/components/typography/PageTitle"
 import SectionTitle from "@/components/typography/SectionTitle"
 import UserAvatar from "@/components/UserAvatar"
 import { UserProvider } from "@/providers/UserProvider"
+import { FeedbackType } from "@/types"
 import { UserType } from "@/types/User"
 import { useTranslations } from "next-intl"
 
@@ -19,6 +21,17 @@ const Showcase = () => {
     imageUrl: "https://xsgames.co/randomusers/avatar.php?g=male",
     created_at: new Date(),
     updated_at: new Date()
+  }
+
+  const feedback: FeedbackType = {
+    id: 1,
+    user: fakeUser,
+    created_at: new Date(),
+    event: {
+      id: 1,
+      title: "Start presentation"
+    },
+    content: "Improvement could be incorporating more real-world examples to illustrate the points. Nevertheless, it was a compelling presentation."
   }
 
   return (
@@ -46,12 +59,14 @@ const Showcase = () => {
         </div>
 
         <div className="flex flex-col">
-          <StarRating rating={1.8} />
+          <StarRating rating={1.8} /> 1.8
           <StarRating rating={3.25} />
           <StarRating rating={3.5} />
           <StarRating rating={3.75} />
           <StarRating rating={4} />
         </div>
+
+        <FeedbackCard feedback={feedback} user={fakeUser} />
       </div>
     </UserProvider>
   )
---
+++ src/components/FeedbackCard.tsx
@@ -0,0 +1,29 @@
+import { UserType } from "@/types/User"
+import UserAvatar from "./UserAvatar"
+import { FeedbackType } from "@/types"
+import { useFormatter, useTranslations } from "next-intl"
+
+export function FeedbackCard({ feedback, user }: { feedback: FeedbackType; user: UserType }) {
+  const t = useTranslations("general")
+  const format = useFormatter()
+  return (
+    <div className="border rounded-lg p-4">
+      {/* Header */}
+      <div className="flex mb-2 items-center justify-between">
+        <div className="flex gap-4">
+          <UserAvatar user={user} />
+          <div>
+            <span className="font-semibold">{feedback.user.id === user.id ? t("you") : feedback.user.name}</span>
+            <p className="text-sm text-muted-foreground">{feedback.user.role}</p>
+          </div>
+        </div>
+        <p className="text-muted-foreground text-sm">{format.dateTime(feedback.created_at, { year: "numeric", month: "short", day: "numeric" })}</p>
+      </div>
+      {/* Content */}
+      <div>
+        <span className="font-sans font-bold text-lg">{feedback.event.title}</span>
+        <p>{feedback.content}</p>
+      </div>
+    </div>
+  )
+}
