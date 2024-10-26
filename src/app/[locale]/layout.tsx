d5f8872de8677df5d613c8681907d1a6e78a5237
DavidHoen <david.hoen10@gmail.com>
2024-10-26 11:23:36 +0200

    Upgraded to Next15

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -32,7 +32,17 @@ export const metadata: Metadata = {
   }
 }
 
-export default async function RootLayout({ children, params: { locale } }: { children: React.ReactNode; params: { locale: string } }) {
+export default async function RootLayout(props: { children: React.ReactNode; params: Promise<{ locale: string }> }) {
+  const params = await props.params;
+
+  const {
+    locale
+  } = params;
+
+  const {
+    children
+  } = props;
+
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale as any)) {
     notFound()
