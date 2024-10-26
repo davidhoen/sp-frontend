8ef18d373ef780bd76066fd64f0100d9cabe7319
DavidHoen <david.hoen10@gmail.com>
2024-10-26 11:00:04 +0200

    Refactor typography component folder structure

--- src/components/typography/PageTitle.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react"
-
-const PageTitle = ({ children }: { children: ReactNode }) => {
-  return <h1 className="font-sans font-bold text-2xl border-b-2 border-primary w-fit">{children}</h1>
-}
-
-export default PageTitle
