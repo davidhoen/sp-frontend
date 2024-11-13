5f60758896cb4bc0ba3d6ead5fa9172e90a854ef
DavidHoen <david.hoen10@gmail.com>
2024-11-13 15:32:38 +0100

    Remove unused middleware logic and add environment variable declaration for backend URL

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,6 @@
 import createMiddleware from "next-intl/middleware"
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
 import { routing } from "./i18n/routing"
 export default createMiddleware(routing)
-
-export async function middleware(req: NextRequest) {
-
-  const locale = req.cookies.get("NEXT_LOCALE")?.value || "en"
-  const session_token = req.cookies.get("skills_passport_session")?.value
-
-  if (req.nextUrl.pathname === "/" && session_token) {
-    return NextResponse.redirect(new URL(`/${locale}/student`, req.url))
-  }
-
-  return NextResponse.next()
-}
 
 export const config = {
   // Match only internationalized pathnames
