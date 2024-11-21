69a1f38115812b50a098314b5971eda9f4143a08
DavidHoen <david.hoen10@gmail.com>
2024-11-21 13:48:14 +0100

    Added compentency detail page

---
+++ src/app/[locale]/(authenticated)/student/competencies/[id]/page.tsx
@@ -0,0 +1,63 @@
+import ProfileTile from "@/components/ProfileTile"
+import StarRating from "@/components/StarRating"
+import PageTitle from "@/components/Typography/PageTitle"
+import SectionTitle from "@/components/Typography/SectionTitle"
+import { getCompetencyRating, getMostRecentRating } from "@/lib"
+import { fakeCompetency, fakeSkill, fakeSkill2 } from "@/lib/fakeData"
+import { getCompetency } from "@/lib/queries"
+import { getTranslations } from "next-intl/server"
+
+const CompetencyDetail = async ({ params }: { params: { id: number } }) => {
+    const t = await getTranslations("general")
+    let competency = await getCompetency(params.id)
+
+    if (!competency)
+        competency = { ...fakeCompetency, skills: [fakeSkill, fakeSkill2] }
+
+    // if (!competency)
+    //     return t("noSkillFound")
+
+    const avgRating = getCompetencyRating(competency)
+
+    return <div className="flex flex-col gap-2">
+
+        {/* Title and profiles */}
+        <div className="flex justify-between">
+            <PageTitle>{competency.title}</PageTitle>
+            <div className="flex gap-2">
+                {competency?.profiles.map((profile) => <ProfileTile key={profile.id} profile={profile} variant="icon" />)}
+            </div>
+        </div>
+
+        {/* Description */}
+        <p>{competency?.desc}</p>
+
+        {/* Rating */}
+        <div className="my-4">
+            <SectionTitle>{t("rating")}</SectionTitle>
+
+            <div className="flex gap-4">
+                {/* Star rating */}
+                <StarRating rating={avgRating || 0} />
+            </div>
+        </div>
+
+        <div>
+            <SectionTitle>{t("overview")}</SectionTitle>
+            <p>{competency?.overview}</p>
+        </div>
+
+        {/* Related skills */}
+        <div className="mt-6">
+            <SectionTitle numberOfItems={competency.skills?.length}>{t("skills")}</SectionTitle>
+            <div className="grid gap-4 max-w-xs">
+                {competency?.skills.map((skill) => <div className="flex justify-between" key={skill.id}>
+                    <span className="">{skill.title}</span>
+                    <StarRating rating={getMostRecentRating(skill.ratings)?.rating || 0} />
+                </div>)}
+            </div>
+        </div>
+    </div>
+}
+
+export default CompetencyDetail
