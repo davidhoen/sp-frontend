c6e4189d9ad807dc3925d99ce41d997260781474
DavidHoen <david.hoen10@gmail.com>
2024-12-03 11:26:10 +0100

    Updated read me and fixed merge issues

--- src/app/[locale]/(authenticated)/student/_components/DashboardButtons.tsx
+++ src/app/[locale]/(authenticated)/student/_components/DashboardButtons.tsx
@@ -1,15 +1,10 @@
-"use client"
-
 import { Button } from "@/components/ui/button"
 import { Link } from "@/i18n/routing"
-import { useUser } from "@/providers/UserProvider"
 import { PlusIcon, UserIcon } from "lucide-react"
 
 export const DashboardButtons = () => {
-    const { basePath } = useUser()
-
     return <div className="flex gap-2">
-        <Link href={{ pathname: `${basePath}/skills/`, query: "is_added=false" }}>
+        <Link href={{ pathname: `/student/skills/`, query: "is_added=false" }}>
             <Button className="rounded-full"><PlusIcon size={16} />Add a skill</Button>
         </Link>
         <Button className="rounded-full" variant="outline"><UserIcon size={16} />Public profile</Button>
