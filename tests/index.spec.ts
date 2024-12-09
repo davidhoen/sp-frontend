8822eec7d52688500b2e2a331613955cd9434f9a
DavidHoen <david.hoen10@gmail.com>
2024-12-09 12:15:45 +0100

    Updated test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,30 +1,29 @@
 import { test, expect } from '@playwright/test';
 
 test('Change language', async ({ page }) => {
-  await page.goto('/nl/login/');
-  await page.getByLabel('Email').click();
-  await page.getByLabel('Email').fill('admin@sp');
-  await page.getByLabel('Password').click();
-  await page.getByLabel('Password').fill('password');
-  await page.getByRole('checkbox').check();
-  await page.getByRole('button', { name: 'Login' }).click();
 
-  // Expect url to contain the default language nl
-  expect(page.url()).toContain('/nl/');
-  await page.getByRole('button').nth(1).click();
+  await page.goto('http://localhost:3000/nl/login/');
+  await page.getByPlaceholder('Your email').click();
+  await page.getByPlaceholder('Your email').fill('student@sp.nl');
+  await page.getByPlaceholder('Your password').click();
+  await page.getByPlaceholder('Your password').fill('password');
+  await page.getByRole('button', { name: 'Log in' }).click();
+
+  // Wait a little
+  await page.waitForTimeout(3000);
+
+  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+  await page.locator('header').getByRole('button').nth(1).click();
   await page.getByText('English').click();
+  await expect(page.locator('#Competencies')).toContainText('Competencies');
+  await page.locator('header').getByRole('button').nth(1).click();
+  await page.getByText('Nederlands').click();
+  await expect(page.locator('#Competenties')).toContainText('Competenties');
+  await page.locator('header').getByRole('button').nth(1).click();
+  await page.getByText('Uitloggen').click();
 
   // Wait a little
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(3000);
 
-  // Expect url to contain the new language en
-  expect(page.url()).toContain('/en/');
-
-  await page.getByRole('button').nth(1).click();
-  await page.getByRole('menuitem', { name: 'nl Nederlands' }).click();
-
-  // Wait a little
-  await page.waitForTimeout(1000);
-
-  expect(page.url()).toContain('/nl/');
+  await expect(page.getByRole('button')).toContainText('Log in');
 });
