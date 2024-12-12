fa6b2229120d0567982759d702bb7a90779d8a78
DavidHoen <david.hoen10@gmail.com>
2024-12-12 16:11:03 +0100

    Removed refresh wrapper

--- src/app/[locale]/(authenticated)/student/skills/[id]/page.tsx
+++ src/app/[locale]/(authenticated)/student/skills/[id]/page.tsx
@@ -1,17 +1,16 @@
-import { EndorsementsList } from "@/components/EndorsementsList"
-import { FeedbacksList } from "@/components/FeedbacksList"
-import UpdateRatingModal from "@/components/Modals/UpdateRatingModal"
-import ProfileTile from "@/components/ProfileTile"
-import RefreshWrapper from "@/components/RefreshWrapper";
-import StarRating from "@/components/StarRating"
-import TimeLineWithUser from "@/components/Timeline/TimeLineWithUser"
-import PageTitle from "@/components/Typography/PageTitle"
-import SectionTitle from "@/components/Typography/SectionTitle"
-import { Link } from "@/i18n/routing"
-import { getSkill } from "@/lib/queries/server/queries"
-import { PencilIcon } from "lucide-react"
-import { getTranslations } from "next-intl/server"
-import { notFound } from "next/navigation"
+import { EndorsementsList } from "@/components/EndorsementsList";
+import { FeedbacksList } from "@/components/FeedbacksList";
+import UpdateRatingModal from "@/components/Modals/UpdateRatingModal";
+import ProfileTile from "@/components/ProfileTile";
+import StarRating from "@/components/StarRating";
+import TimeLineWithUser from "@/components/Timeline/TimeLineWithUser";
+import PageTitle from "@/components/Typography/PageTitle";
+import SectionTitle from "@/components/Typography/SectionTitle";
+import { Link } from "@/i18n/routing";
+import { getSkill } from "@/lib/queries/server/queries";
+import { PencilIcon } from "lucide-react";
+import { getTranslations } from "next-intl/server";
+import { notFound } from "next/navigation";
 
 const SkillsDetail = async ({ params }: { params: { id: number } }) => {
     const t = await getTranslations("general")
@@ -19,60 +18,58 @@ const SkillsDetail = async ({ params }: { params: { id: number } }) => {
     const skill = await getSkill(params.id)
     if (!skill) notFound()
 
-    return <RefreshWrapper>
-        <div className="flex flex-col gap-2">
+    return <div className="flex flex-col gap-2">
 
-            {/* Title and profiles */}
-            <div className="flex justify-between">
-                <PageTitle>{skill.title}</PageTitle>
-                <div className="flex gap-2">
-                    {skill?.competency?.profiles?.map((profile) => <ProfileTile key={profile.id} profile={profile} variant="icon" />)}
-                </div>
-            </div>
-
-            {/* Description */}
-            <p>{skill?.desc}</p>
-
-            {/* Rating */}
-            <div className="mt-4">
-                <SectionTitle>{t("rating")}</SectionTitle>
-
-                <div className="flex gap-4">
-                    {/* Star rating */}
-                    <StarRating rating={skill.rating || 0} />
-
-                    {/* Edit rating */}
-                    {skill.is_added && <UpdateRatingModal currentRating={skill.rating} skillId={skill.id}>
-                        <div className="flex items-center bg-border p-1 rounded-full"><PencilIcon size={15} /></div>
-                    </UpdateRatingModal>}
-                </div>
-            </div>
-
-            {/* Skill journey */}
-            <div>
-                <SectionTitle>{t("yourJourney")}</SectionTitle>
-                <TimeLineWithUser skillId={skill.id} />
-            </div>
-
-            {/* Feedbacks */}
-            <div className="mt-6">
-                <FeedbacksList skillId={skill.id} />
-            </div>
-
-            {/* Endorsements */}
-            <div className="mt-6 mb-4">
-                <EndorsementsList skillId={skill.id} />
-            </div>
-
-            {/* Competencies */}
-            <span className="font-bold">{t("competencies")}</span>
+        {/* Title and profiles */}
+        <div className="flex justify-between">
+            <PageTitle>{skill.title}</PageTitle>
             <div className="flex gap-2">
-                {skill.competency && <Link href={`/student/competencies/${skill.competency.id}`}>
-                    <div className="bg-sidebar-accent px-4 py-1 rounded-full">{skill.competency.title}</div>
-                </Link>}
+                {skill?.competency?.profiles?.map((profile) => <ProfileTile key={profile.id} profile={profile} variant="icon" />)}
             </div>
         </div>
-    </RefreshWrapper>
+
+        {/* Description */}
+        <p>{skill?.desc}</p>
+
+        {/* Rating */}
+        <div className="mt-4">
+            <SectionTitle>{t("rating")}</SectionTitle>
+
+            <div className="flex gap-4">
+                {/* Star rating */}
+                <StarRating rating={skill.rating || 0} />
+
+                {/* Edit rating */}
+                {skill.is_added && <UpdateRatingModal currentRating={skill.rating} skillId={skill.id}>
+                    <div className="flex items-center bg-border p-1 rounded-full"><PencilIcon size={15} /></div>
+                </UpdateRatingModal>}
+            </div>
+        </div>
+
+        {/* Skill journey */}
+        <div>
+            <SectionTitle>{t("yourJourney")}</SectionTitle>
+            <TimeLineWithUser skillId={skill.id} />
+        </div>
+
+        {/* Feedbacks */}
+        <div className="mt-6">
+            <FeedbacksList skillId={skill.id} />
+        </div>
+
+        {/* Endorsements */}
+        <div className="mt-6 mb-4">
+            <EndorsementsList skillId={skill.id} />
+        </div>
+
+        {/* Competencies */}
+        <span className="font-bold">{t("competencies")}</span>
+        <div className="flex gap-2">
+            {skill.competency && <Link href={`/student/competencies/${skill.competency.id}`}>
+                <div className="bg-sidebar-accent px-4 py-1 rounded-full">{skill.competency.title}</div>
+            </Link>}
+        </div>
+    </div>
 }
 
 export default SkillsDetail
