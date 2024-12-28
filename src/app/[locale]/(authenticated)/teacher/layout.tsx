b486a3e8545c3a1012651e02ac31c7d5c52d1fe0
David Hoen <david.hoen10@gmail.com>
2024-12-28 16:24:57 +0100

    Added competencies overview

--- src/app/[locale]/(authenticated)/teacher/layout.tsx
+++ src/app/[locale]/(authenticated)/teacher/layout.tsx
@@ -12,6 +12,8 @@ const StudentLayout = async ({ children }: { children: ReactNode }) => {
     { title: t("groups"), url: `/teacher/groups`, icon: "Group" },
     { title: t("skills"), url: `/teacher/skills`, icon: "Star" },
     { title: t("students"), url: `/teacher/students`, icon: "Users" },
+    { title: t("competencies"), url: `/teacher/competencies`, icon: "Target", adminOnly: true },
+    { title: t("profiles"), url: `/teacher/profiles`, icon: "GraduationCap", adminOnly: true },
   ]
 
   return <SideBar items={menuItems}>
