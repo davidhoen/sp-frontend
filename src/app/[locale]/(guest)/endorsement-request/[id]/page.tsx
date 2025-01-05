e2b09dbaa2c42f1dc6406d061de01e149392cc20
David Hoen <david.hoen10@gmail.com>
2025-01-05 13:37:32 +0100

    Added confirmation page to endorsement request

--- src/app/[locale]/(guest)/endorsement-request/[id]/page.tsx
+++ src/app/[locale]/(guest)/endorsement-request/[id]/page.tsx
@@ -5,7 +5,6 @@ import PageTitle from "@/components/Typography/PageTitle";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import UserAvatar from "@/components/UserAvatar";
 import { getFullName } from "@/lib";
-import { fakeEndorsementRequest } from "@/lib/fakeData";
 import { getEndorsementRequestResponse } from "@/lib/queries/server/queries";
 import { getTranslations } from "next-intl/server";
 import { notFound } from "next/navigation";
@@ -22,16 +21,16 @@ const EndorsementRequestPage = async (props: { params: Promise<{ id: string }> }
     return <div className="container mx-auto max-w-2xl md:py-8">
         <Card>
             <CardHeader className="space-y-1">
-                <div className="flex flex-col-reverse md:flex-row gap-6 md:items-center justify-between w-full mb-6 md:mb-0">
+                <div className="flex flex-col-reverse md:flex-row gap-6 md:items-center justify-between w-full mb-6 md:mb-4">
                     <CardTitle>
-                        <PageTitle>{t("endorsementRequest.title")}</PageTitle>
+                        <PageTitle className="">{t("endorsementRequest.title")}</PageTitle>
                     </CardTitle>
                     <Logo className="w-fit" />
                 </div>
             </CardHeader>
             <CardContent>
                 {(endorsementRequest === "expired") ?
-                    <div className="text-center">{t("endorsementRequest.requestExpired")}</div>
+                    <div className="text-center mt-2">{t("endorsementRequest.requestExpired")}</div>
                     :
                     <>
                         <div className="-mt-8 mb-8">{t("endorsementRequest.endorseUserSkill", { name: getFullName(endorsementRequest.requester) })}</div>
